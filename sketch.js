ece70826c6a1ac95a432da5f42006f7231f470d9
GKedarnath <72261336+GKedarnath@users.noreply.github.com>
2020-11-16 14:24:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,40 +5,42 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 
-var ground
+var ground;
+var tree;
+var boy;
 
 function preload()
 {
-	
+	this.image = loadImage("sprites/tree.png");
+	this.image = loadImage("sprites/boy.png");
 	
 }
 
 function setup() {
 	createCanvas(800, 700);
-	
 
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-	ground = new Ground(400,height,800,20)
+	ground = new Ground(400,height,800,20);
 
-	
 
 	Engine.run(engine);
-
-	
   
 }
 
 
 function draw() {
-	display();
   rectMode(CENTER);
-  background(255);
- 
+  background(0);
+
   ground.display();
+
+  image(tree,400,400100,100);
+  boy(boy,200,200,50,50);
+
 
   
   drawSprites();
